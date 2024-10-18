ae1cda45bfd0df794ffea6efc02e27df49c43556
roland.ambs <roland.ambs@meteoblue.com>
2024-10-18 13:26:37 +0200

    add button

--- account-table.js
+++ account-table.js
@@ -8,11 +8,15 @@ export class AccountTable extends LitElement {
     }
   `;
 
-  static properties = {};
+  static properties = {
+    accounts: { type: Array },
+  };
 
   constructor() {
     super();
+    this.data = [];
   }
+
   _productTask = new Task(this, {
     task: async ([], { signal }) => {
       const response = await fetch(`/api/accounts`, { signal });
@@ -27,31 +31,76 @@ export class AccountTable extends LitElement {
     args: () => [],
   });
 
+  connectedCallback() {
+    super.connectedCallback();
+    this.loadTableData(); // Tabelle wird beim Laden der Komponente initialisiert
+  }
+
+  // Diese Methode lädt die Daten neu
+  async loadTableData() {
+    try {
+      console.log("fetching");
+      const response = await fetch("/api/accounts");
+      if (response.ok) {
+        const jsonData = await response.json();
+        this.data = jsonData; // Daten in der Tabelle speichern
+      } else {
+        console.error("Fehler beim Laden der Tabelle");
+      }
+    } catch (error) {
+      console.error("Fehler:", error);
+    }
+  }
+
+  handleRelead() {
+    this.loadTableData();
+  }
+
   render() {
-    return this._productTask.render({
-      pending: () => html`<p>Loading product...</p>`,
-
-      complete: (accounts) => html`
-        <table>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Betrag</th>
-          </tr>
-          ${accounts.map(
-            (account) => html`
-              <tr>
-                <td>${account.id}</td>
-                <td>${account.kontoname}</td>
-                <td>$${account.betrag.toFixed(2)}</td>
-              </tr>
-            `
-          )}
-        </table>
-      `,
-
-      error: (e) => html`<p>Error: ${e}</p>`,
-    });
+    // return this._productTask.render({
+    //   pending: () => html`<p>Loading product...</p>`,
+    //
+    //   complete: (accounts) => html`
+    //     <button @click="${this.handleRelead}">neu laden</button>
+    //     <table>
+    //       <tr>
+    //         <th>ID</th>
+    //         <th>Name</th>
+    //         <th>Betrag</th>
+    //       </tr>
+    //       ${accounts.map(
+    //         (account) => html`
+    //           <tr>
+    //             <td>${account.id}</td>
+    //             <td>${account.kontoname}</td>
+    //             <td>$${account.betrag.toFixed(2)}</td>
+    //           </tr>
+    //         `
+    //       )}
+    //     </table>
+    //   `,
+    //
+    //   error: (e) => html`<p>Error: ${e}</p>`,
+    // });
+    return html`
+      <button @click="${this.handleRelead}">neu laden</button>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Betrag</th>
+        </tr>
+        ${this.data.map(
+          (account) => html`
+            <tr>
+              <td>${account.id}</td>
+              <td>${account.kontoname}</td>
+              <td>$${account.betrag.toFixed(2)}</td>
+            </tr>
+          `
+        )}
+      </table>
+    `;
   }
 }
 customElements.define("account-table", AccountTable);
