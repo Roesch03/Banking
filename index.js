d141348845e15daf0d96620e56c277dbbd204a50
roland.ambs <roland.ambs@meteoblue.com>
2024-10-21 13:45:39 +0200

    implement transactions

--- index.js
+++ index.js
@@ -23,6 +23,25 @@ async function accounts() {
     .toArray();
 }
 
+async function id_exists(id) {
+  return (
+    (await mongo_client
+      .db("banking")
+      .collection("accounts")
+      .countDocuments({ id: id })) > 0
+  );
+}
+
+async function transaction(from, to, amount) {
+  const accounts = mongo_client.db("banking").collection("accounts");
+  await accounts.updateOne({ id: from }, { $inc: { betrag: -amount } });
+  await accounts.updateOne({ id: to }, { $inc: { betrag: amount } });
+  await mongo_client
+    .db("banking")
+    .collection("transactions")
+    .insertOne({ from: from, to: to, amount: amount });
+}
+
 const server = createServer(async (req, res) => {
   // Parse the request url
   const reqUrl = new URL(
@@ -36,6 +55,36 @@ const server = createServer(async (req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.write(JSON.stringify(await accounts()));
     res.end();
+  } else if (reqPath == "/api/transaction" && req.method === "POST") {
+    var body = "";
+
+    req.on("data", function (data) {
+      body += data;
+
+      // Too much POST data, kill the connection!
+      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+      if (body.length > 1e6) req.connection.destroy();
+    });
+
+    req.on("end", async function () {
+      const trans = JSON.parse(body);
+      if (
+        trans.hasOwnProperty("from") &&
+        trans.hasOwnProperty("to") &&
+        trans.hasOwnProperty("amount") &&
+        (await id_exists(trans.from)) &&
+        (await id_exists(trans.to))
+      ) {
+        await transaction(trans.from, trans.to, trans.amount);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write(JSON.stringify({ success: true }));
+        res.end();
+      } else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.write(JSON.stringify({ success: false }));
+        res.end();
+      }
+    });
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.write("404 Not Found");
