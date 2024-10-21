17127be034992f9c2e5f6c1650e60c09cf3e6423
roland.ambs <roland.ambs@meteoblue.com>
2024-10-21 10:55:27 +0200

    add ongo stuff

--- api_serv.dockerfile
+++ api_serv.dockerfile
@@ -4,5 +4,6 @@ COPY ./*.js ./
 COPY ./*.html ./
 COPY ./*.json ./
 COPY ./dev ./
+RUN npm i
 EXPOSE 8080
 CMD node index.js
--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 import { createServer } from "http";
 import { parse } from "url";
 import { readFileSync } from "node:fs";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-const accounts = JSON.parse(readFileSync("data.json"));
+async function accounts() {
+  const mongo_address = process.env.MONGO_ADDRESS || "localhost";
+  // mongodb://localhost:27017
+  const uri = `mongodb://${mongo_address}:27017/`;
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  const res = await client
+    .db("banking")
+    .collection("accounts")
+    .find()
+    .toArray();
+  // Ensures that the client will close when you finish/error
+  await client.close();
+  return res;
+}
 
-const server = createServer((req, res) => {
+const server = createServer(async (req, res) => {
   // Parse the request url
   const reqUrl = parse(req.url).pathname;
 
   if (reqUrl == "/api/accounts") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify(accounts));
+    res.write(JSON.stringify(await accounts()));
     res.end();
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
